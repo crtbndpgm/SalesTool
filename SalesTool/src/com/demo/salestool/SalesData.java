cce81d84d4c6a4c8019ee286bd914c2837541b31
MICHAELS <michael_stephenson@ooida.com>
2019-03-27 10:57:45 -0500

    Initial checkin of SalesTool

---
+++ SalesTool/src/com/demo/salestool/SalesData.java
@@ -0,0 +1,9 @@
+package com.demo.salestool;
+
+public class SalesData {
+	int data[] = {0, 2, 4, 8, 16, 32};
+	
+	public void display() {
+		System.out.println("TODO: Implement data display");
+	}
+}
