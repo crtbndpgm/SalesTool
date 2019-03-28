ad92a87af9c837c83a609d4929b3e1e5b733e8a4
MICHAELS <michael_stephenson@ooida.com>
2019-03-28 12:47:38 -0500

    Partial implementation of display

--- SalesTool/src/com/demo/salestool/SalesData.java
+++ SalesTool/src/com/demo/salestool/SalesData.java
@@ -4,6 +4,8 @@ public class SalesData {
 	int data[] = {0, 2, 4, 8, 16, 32};
 	
 	public void display() {
-		System.out.println("TODO: Implement data display");
+		for(int i = 0; i < data.length; i++){
+			System.out.println("TODO: Implement data display");
+		}
 	}
 }
