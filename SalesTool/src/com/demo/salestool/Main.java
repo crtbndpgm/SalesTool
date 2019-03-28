9f42255562e29c2be9444d60d324945aee7cec90
MICHAELS <michael_stephenson@ooida.com>
2019-03-28 09:14:27 -0500

    Removed blank line in Main.java

--- SalesTool/src/com/demo/salestool/Main.java
+++ SalesTool/src/com/demo/salestool/Main.java
@@ -1,20 +1,16 @@
 package com.demo.salestool;
 
 public class Main {
-
 	public static void main(String[] args) {
 		SalesData data = new SalesData();
 		// display header
 		displayGreeting();
 		data.display();
 	}
-	
 	private static void displayGreeting() {
 		System.out.println("HELLO HAPPY SALES PEOPLE!");
 		System.out.println("THIS APP SHOWS SALES DATA");
 		System.out.println("test 1");
 		System.out.println("test 2");
-		
 	}
-
 }
