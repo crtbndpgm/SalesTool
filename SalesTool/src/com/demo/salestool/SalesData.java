5bc051dd16aaaadf1d9892376100ac83c8ae6f5c
MICHAELS <michael_stephenson@ooida.com>
2019-03-28 13:46:57 -0500

    Worked on for loop

--- SalesTool/src/com/demo/salestool/SalesData.java
+++ SalesTool/src/com/demo/salestool/SalesData.java
@@ -1,11 +1,12 @@
 package com.demo.salestool;
 
 public class SalesData {
-	int data[] = { -1, 0, 2, 4, 8, 16, 32, 64, 128 };
+	int data[] = { 0, 2, 4, 8, 16, 32, 64, 128 };
 
 	public void display() {
 		System.out.println("Data:");
-		for (int i : data) {
+			for (int i : data) {
+			System.out.println("i: " + i);
 			System.out.println("Next value: " + data[i]);
 		}
 	}
