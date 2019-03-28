816c6e56c31f80b00eae4ced6381cf1ad218e375
MICHAELS <michael_stephenson@ooida.com>
2019-03-28 13:02:49 -0500

    Completed displaying data in a sales data object

--- SalesTool/src/com/demo/salestool/SalesData.java
+++ SalesTool/src/com/demo/salestool/SalesData.java
@@ -1,11 +1,12 @@
 package com.demo.salestool;
 
 public class SalesData {
-	int data[] = {0, 2, 4, 8, 16, 32};
-	
+	int data[] = { 0, 2, 4, 8, 16, 32 };
+
 	public void display() {
-		for(int i = 0; i < data.length; i++){
-			System.out.println("TODO: Implement data display");
+		System.out.println("Data:");
+		for (int i = 0; i < data.length; i++) {
+			System.out.println("Next value: " + data[i]);
 		}
 	}
 }
