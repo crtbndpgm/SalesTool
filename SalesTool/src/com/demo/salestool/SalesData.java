5d5c4dde1490e0d8a773d821bd358057116e9c3b
MICHAELS <michael_stephenson@ooida.com>
2019-03-28 13:21:42 -0500

    Changed defaults

--- SalesTool/src/com/demo/salestool/SalesData.java
+++ SalesTool/src/com/demo/salestool/SalesData.java
@@ -1,11 +1,11 @@
 package com.demo.salestool;
 
 public class SalesData {
-	int data[] = { 0, 2, 4, 8, 16, 32 };
+	int data[] = { -1, 0, 2, 4, 8, 16, 32, 64, 128 };
 
 	public void display() {
 		System.out.println("Data:");
-		for (int i = 0; i < data.length; i++) {
+		for (int i : data) {
 			System.out.println("Next value: " + data[i]);
 		}
 	}
